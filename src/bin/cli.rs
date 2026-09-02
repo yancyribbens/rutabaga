4bb804ec62e7e5e29e8da7b28b7fb7d6cf7607d0
yancy <github@yancy.lol>
2026-09-02 11:26:36 -0500

    write secret key to file

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -2,6 +2,8 @@ use bitcoin::secp256k1::rand;
 use bitcoin::secp256k1::Secp256k1;
 use bitcoin::{Address, Network};
 use clap::Parser;
+use std::fs::File;
+use std::io::Write;
 use std::path::PathBuf;
 
 #[derive(clap::Parser)]
@@ -34,12 +36,15 @@ fn main() {
     let cli = Args::parse();
 
     match cli.commands {
-        Commands::Wallet(WalletCmd::GenerateAddress { out: _ }) => {
+        Commands::Wallet(WalletCmd::GenerateAddress { out }) => {
             let s = Secp256k1::new();
-            let (_, pub_key) = s.generate_keypair(&mut rand::thread_rng());
+            let (priv_key, pub_key) = s.generate_keypair(&mut rand::thread_rng());
             let (internal_key, _parity) = pub_key.x_only_public_key();
             let address = Address::p2tr(&s, internal_key, None, Network::Signet);
             println!("{:?}", address);
+
+            let mut file = File::create_new(out.unwrap()).unwrap();
+            file.write_all(&priv_key.secret_bytes()).unwrap();
         }
         Commands::Wallet(WalletCmd::PrintKeysFromKeysFile { path: _ }) => {
             todo!()
