1bb5ab0061739c26bdd95760b982e9d87c8ef58f
yancy <github@yancy.lol>
2026-09-03 19:55:05 -0500

    wip: scan chain for wallet address

--- src/wallet.rs
+++ src/wallet.rs
@@ -11,6 +11,9 @@ use bitcoin::transaction::Version;
 use bitcoin::absolute::LockTime;
 use bitcoin::Address;
 use std::fmt;
+use bitcoinkernel::core::TransactionExt;
+use bitcoinkernel::core::TxOutExt;
+use bitcoinkernel::core::ScriptPubkeyExt;
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -57,10 +60,24 @@ impl Wallet {
 
     pub fn scan_block(
         &mut self,
-        _kernel_block: bitcoinkernel::Block,
+        kernel_block: bitcoinkernel::Block,
         _spent_outputs: bitcoinkernel::BlockSpentOutputs,
         _block_height: u32,
     ) -> usize {
+
+        // ignore first transaction in block as coin-base
+        for tx in kernel_block.transactions().skip(1) {
+            for out in tx.outputs() {
+                let _val = out.value();
+                let script_pubkey = out.script_pubkey();
+                let _bytes = script_pubkey.to_bytes();
+
+                // TODO
+                // create bitcoin script_buf using new_tr(secp, pub_key, None)
+                // then compare the bytes to see if its a match
+            }
+        }
+
         0
     }
     
