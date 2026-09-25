03a084e5af0d013add3dfe2920bed85eacd7036a
yancy <github@yancy.lol>
2026-09-25 10:36:01 -0500

    wip

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -83,7 +83,7 @@ enum WalletCmd {
     },
 }
 
-fn build_tx(coins: Vec<Coin>, recipient: ScriptBuf, kp: Keypair, fee_rate: FeeRate) -> Transaction {
+fn build_tx_no_change(coins: Vec<Coin>, recipient: ScriptBuf, kp: Keypair, fee_rate: FeeRate) -> Transaction {
     let inputs = coins
         .iter()
         .map(|coin| TxIn {
@@ -114,8 +114,81 @@ fn build_tx(coins: Vec<Coin>, recipient: ScriptBuf, kp: Keypair, fee_rate: FeeRa
     let fee: Amount = FeeRate::fee_wu(fee_rate, tx.weight()).unwrap();
 
     // now that the fee is known, update the transaction to include a fee.
-    let output = TxOut { value: value - fee, script_pubkey: recipient.clone() };
+    let send_amount = value - fee;
+    let output = TxOut { value: send_amount, script_pubkey: recipient.clone() };
     tx.output = vec![output];
+
+    let mut sighasher = SighashCache::new(&mut tx);
+    for (index, _) in coins.clone().into_iter().enumerate() {
+        let sighash_type = TapSighashType::Default;
+        //let prevouts = vec![tx_out];
+        let prevouts = Prevouts::All(&prevouts);
+
+        let sighash =
+            sighasher.taproot_key_spend_signature_hash(index, &prevouts, sighash_type).unwrap();
+
+        let s = Secp256k1::new();
+        let tweaked: TweakedKeypair = kp.tap_tweak(&s, None);
+        let msg = Message::from_digest(sighash.to_byte_array());
+        let signature = s.sign_schnorr(&msg, &tweaked.to_keypair());
+
+        let signature = bitcoin::taproot::Signature { signature, sighash_type };
+        sighasher.witness_mut(index).unwrap().push(signature.to_vec());
+    }
+    let tx = sighasher.into_transaction();
+    tx.to_owned()
+}
+
+fn build_tx_with_change(coins: Vec<Coin>, target: Amount, recipient: ScriptBuf, change_addr: ScriptBuf, kp: Keypair, fee_rate: FeeRate) -> Transaction {
+    let inputs = coins
+        .iter()
+        .map(|coin| TxIn {
+            previous_output: coin.outpoint,
+            script_sig: ScriptBuf::new(),
+            sequence: Sequence::ENABLE_RBF_NO_LOCKTIME,
+            witness: Witness::default(),
+        })
+        .collect();
+
+    let prevouts: Vec<_> = coins.iter().map(|coin| coin.tx_out.clone()).collect();
+    let value = prevouts
+        .iter()
+        .map(|tx_out| tx_out.value)
+        .try_fold(Amount::ZERO, Amount::checked_add)
+        .unwrap();
+
+    let output = TxOut { value, script_pubkey: recipient.clone() };
+
+    //let mut tx_test = Transaction {
+        //version: Version::TWO,
+        //lock_time: LockTime::ZERO,
+        //input: inputs,
+        //output: vec![output.clone(), output],
+    //};
+
+    // now that the weight is known, the fee can be calculated.
+    //let fee: Amount = FeeRate::fee_wu(fee_rate, tx_test.weight()).unwrap();
+    //panic!("fee {:?} weight {:?}", fee, tx_test.weight());
+
+    let mut tx = Transaction {
+        version: Version::TWO,
+        lock_time: LockTime::ZERO,
+        input: inputs,
+        output: vec![output],
+    };
+
+    // now that the weight is known, the fee can be calculated.
+    let fee: Amount = FeeRate::fee_wu(fee_rate, tx.weight()).unwrap();
+
+    // now that the fee is known, update the transaction to include a fee.
+    let change = value - target - fee;
+    println!("send amount {:?} target {:?} change {:?}", value, target, change);
+
+    let output = TxOut { value: target, script_pubkey: recipient.clone() };
+    let change_output = TxOut { value: change, script_pubkey: change_addr };
+
+    tx.output = vec![output, change_output];
+    println!("final weight {:?}", fee, tx.weight());
 
     let mut sighasher = SighashCache::new(&mut tx);
     for (index, _) in coins.clone().into_iter().enumerate() {
@@ -271,7 +344,7 @@ fn main() {
 
             let address: Address =
                 Address::from_str(&addr).unwrap().require_network(Network::Signet).unwrap();
-            let tx = build_tx(coin_vec, address.script_pubkey(), kp, bitcoin_fee_rate);
+            let tx = build_tx_no_change(coin_vec, address.script_pubkey(), kp, bitcoin_fee_rate);
             let builder = Builder::new("https://blockstream.info/signet/api");
             let blocking_client = builder.build_blocking();
             let response = blocking_client.broadcast(&tx).unwrap();
@@ -299,7 +372,7 @@ fn main() {
                 .collect();
             let address: Address =
                 Address::from_str(&addr).unwrap().require_network(Network::Signet).unwrap();
-            let tx = build_tx(outs, address.script_pubkey(), kp, bitcoin_fee_rate);
+            let tx = build_tx_no_change(outs, address.script_pubkey(), kp, bitcoin_fee_rate);
             let builder = Builder::new("https://blockstream.info/signet/api");
             let blocking_client = builder.build_blocking();
             let response = blocking_client.broadcast(&tx).unwrap();
@@ -313,17 +386,23 @@ fn main() {
             addr,
             fee_rate,
         }) => {
-            let bitcoin_amount = Amount::from_sat(amount);
+            // cost of tx with two output and no inputs
+            let tx_cost = Amount::from_sat(548);
+            let target = Amount::from_sat(amount);
+            let total_target = target + tx_cost;
+
             let bitcoin_fee_rate = FeeRate::from_sat_per_vb_u32(fee_rate);
             let discard_fee_rate = DEFAULT_DISCARD_FEE_RATE;
             let lt_fee_rate = DEFAULT_LONG_TERM_FEE_RATE;
 
             let coins = coin::from_ledger(&outs_ledger, &spent_ledger);
+            println!("{:#?}", coins);
             let cost_of_change = default_tr_cost_of_change(bitcoin_fee_rate, discard_fee_rate);
 
             let (_, selection) =
-                select_coins(bitcoin_amount, cost_of_change, bitcoin_fee_rate, lt_fee_rate, &coins)
+                select_coins(total_target, cost_of_change, bitcoin_fee_rate, lt_fee_rate, &coins)
                     .unwrap();
+            println!("selection {:?}", selection);
             let to_spend = selection.into_iter().cloned().collect();
             let address: Address =
                 Address::from_str(&addr).unwrap().require_network(Network::Signet).unwrap();
@@ -332,8 +411,15 @@ fn main() {
             let bytes: Vec<u8> = fs::read(&keys_path).unwrap();
             let sk = SecretKey::from_slice(&bytes).unwrap();
             let kp = Keypair::from_secret_key(&s, &sk);
+            let sender_script_pub_key = ScriptBuf::new_p2tr(&s, kp.x_only_public_key().0, None);
 
-            let tx = build_tx(to_spend, address.script_pubkey(), kp, bitcoin_fee_rate);
+            let tx = build_tx_with_change(to_spend, target, address.script_pubkey(), sender_script_pub_key, kp, bitcoin_fee_rate);
+            //let inputs = tx.input;
+            //let outputs = tx.output;
+            //println!("input count: {:?} output count: {:?}", inputs.len(), outputs.len());
+            //for o in outputs {
+                //println!("output val: {:?}", o.value);
+            //}
             let builder = Builder::new("https://blockstream.info/signet/api");
             let blocking_client = builder.build_blocking();
             let response = blocking_client.broadcast(&tx).unwrap();
